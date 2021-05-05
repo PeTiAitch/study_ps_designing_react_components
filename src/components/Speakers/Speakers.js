eb368e53182f123577de265adffc6002d4c49ddd
plam <vvillbes@gmail.com>
2021-05-05 14:10:41 +0300

    02_04 Render Components Based on an Array of Data

--- src/components/Speakers/Speakers.js
+++ src/components/Speakers/Speakers.js
@@ -1,6 +1,26 @@
-function Speakers() {
+const Speakers = () => {
+    const speakers = [
+    {
+      imageSrc: 'speaker-component-1124',
+      name: 'Douglas Crockford',
+    },
+    {
+      imageSrc: 'speaker-component-1530',
+      name: 'Tamara Baker',
+    },
+    {
+      imageSrc: 'speaker-component-10803',
+      name: 'Eugene Chuvyrov',
+    },
+  ];
   return (
-    <img src="/images/speakers.png" />
+      <div>
+      {speakers.map(({ imageSrc, name }) => {
+        return (
+          <img src={`/images/${imageSrc}.png`} alt={name} key={imageSrc}></img>
+        );
+      })}
+    </div>
   );
 }
 export default Speakers;
