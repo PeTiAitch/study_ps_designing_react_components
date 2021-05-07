0afde4cc22123bf9ff5f753d1c0c3d4eef1d3f32
plam <vvillbes@gmail.com>
2021-05-07 16:16:20 +0300

    04_01 Simplifying the Speakers Component

--- src/components/SpeakerSearchBar/SpeakerSearchBar.js
+++ src/components/SpeakerSearchBar/SpeakerSearchBar.js
@@ -1,6 +1,16 @@
-function SpeakerSearchBar() {
+import React from "react";
+
+const SpeakerSearchBar = () => {
   return (
-    <img src="/images/searchbar.gif" />
+    <div className="mb-6 ">
+      <input
+        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        id="username"
+        type="text"
+        placeholder="Search by name"
+      />
+    </div>
   );
-}
+};
+
 export default SpeakerSearchBar;
